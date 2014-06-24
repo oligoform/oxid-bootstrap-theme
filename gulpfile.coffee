766add955c89faf7a6f68d2364f6c0ce22484af3
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2014-06-24 22:44:53 +0300

    Add scaffolds for gulp builds

--- gulpfile.coffee
+++ gulpfile.coffee
@@ -1,1 +1,23 @@
 gulp = require 'gulp'
+
+# Load plugins
+$ = require('gulp-load-plugins')()
+
+gulp.task 'static', ->
+  gulp.src('src/static/**')
+    .pipe(gulp.dest('dist'))
+
+gulp.task 'fonts', ->
+  $.bowerFiles()
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('dist/out/bootstrap/src/fonts'))
+    .pipe($.size())
+
+gulp.task 'clean', ->
+  gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
+
+gulp.task 'build', ['static', 'fonts']
+
+gulp.task 'default', ->
+  gulp.start 'build'
---
+++ src/static/application/views/bootstrap/theme.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of OXID Bootstrap theme.
+ *
+ * OXID Bootstrap theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID Bootstrap theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID Console.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Eligijus Vitkauskas
+ * @link   https://github.com/EllisV/oxid-bootstrap-theme
+ */
+
+/**
+ * Theme Information
+ */
+$aTheme = array(
+    'id'          => 'bootstrap',
+    'title'       => 'Bootstrap',
+    'description' => 'Twitter Bootstrap theme for OXID',
+    'thumbnail'   => 'theme.jpg',
+    'version'     => '0.0.1',
+    'author'      => 'Eligijus Vitkauskas'
+);
